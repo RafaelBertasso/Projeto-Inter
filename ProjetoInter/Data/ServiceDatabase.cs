d3a26c882ad1ebca44a222c6373e8a760ad10356
Bowie <Rafaelbertasso06@gmail.com>
2024-10-02 14:49:25 -0300

    criação da classe para uso no banco de dados

---
+++ ProjetoInter/Data/ServiceDatabase.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoInter.Models;
+
+public class ServiceDatabase : DbContext
+{
+    public ServiceDatabase(DbContextOptions options) : base(options) {}
+
+    public DbSet<Service> Services{ get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserService> UserServices { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+}
