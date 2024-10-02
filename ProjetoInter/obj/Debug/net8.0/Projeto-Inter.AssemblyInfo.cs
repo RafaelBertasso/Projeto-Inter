a92d8277c53b7f400fb5431811cef4b5ed1293fe
Bowie <Rafaelbertasso06@gmail.com>
2024-10-02 15:19:35 -0300

    criação das classes controllers

--- ProjetoInter/obj/Debug/net8.0/Projeto-Inter.AssemblyInfo.cs
+++ ProjetoInter/obj/Debug/net8.0/Projeto-Inter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Inter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ba72a47da632f3d54259f470e79e6ace940fe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a26c882ad1ebca44a222c6373e8a760ad10356")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Inter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Inter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
