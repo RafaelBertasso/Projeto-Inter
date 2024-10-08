c03603529f58501e8fcb524ec9c05c7d866ad8ca
Bowie <Rafaelbertasso06@gmail.com>
2024-10-08 13:42:51 -0300

    atualizações finais

--- ProjetoInter/obj/Debug/net8.0/Projeto-Inter.AssemblyInfo.cs
+++ ProjetoInter/obj/Debug/net8.0/Projeto-Inter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Inter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575ea28d1ca69477f8ac96ddedcb258bba2392e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63bafe1875b7f84cb528a5251c9348574ba92857")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Inter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Inter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
